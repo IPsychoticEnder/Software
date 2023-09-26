0773f3bf8675ba84475dd1fff8e00f737cf7a591
Aaron Boekhoudt <95099604+IPsychoticEnder@users.noreply.github.com>
2023-09-26 18:13:08 +0200

    InformTheBossV2

--- WeekThree/InformTheBossV2/InformTheBoss.py
+++ WeekThree/InformTheBossV2/InformTheBoss.py
@@ -1,21 +1,29 @@
 from my_functions import store_functions
 
-while True :
-    store_functions.get_options()
-    user_input = input("> ")
-    if user_input == "s" or user_input == "S":
-        name = input("name: ")
-        price = float(input("price: "))
-        quantity = int(input("quantity: "))
-        store_functions.add_item(name, price, quantity)
+while True:
+	store_functions.get_options()
+	user_input = input("> ")
 
-    elif user_input == "i" or user_input == "I":
-        print("What information would you like? ")
-        store_functions.get_sold_items_list()
-        store_functions.get_total_sale()
-        user_input = input("> ")
-
-    elif user_input == "exit" or user_input == "Exit":
-        break
-    else :
-        print("Try again")
+	if user_input == "s" or user_input == "S":
+		name = input("Name: ")
+		if name in store_functions.get_list_items():
+			quantity = int(input("Quantity:  "))
+			store_functions.add_item(name, 0, quantity)
+		else :
+			price = float(input("Price: "))
+			quantity = int(input("Quantity:  "))
+			store_functions.add_item(name, price, quantity)
+	elif user_input == "i" or user_input == "I":
+		if len(store_functions.get_list_items()) == 0:
+			print("There has not yet been any sales.")
+		else:
+			print("All items sold:")
+			for item in store_functions.get_list_items():
+				print(item, end=" ")
+			print("")
+			print("Total sales:")
+			print(store_functions.get_total_sales())
+	elif user_input == "exit" or user_input == "Exit":
+		break
+	else:
+		print(f"Sorry, the command ({user_input}) is unrecognised.")
--- WeekThree/InformTheBossV2/my_functions/store_functions.py
+++ WeekThree/InformTheBossV2/my_functions/store_functions.py
@@ -1,29 +1,33 @@
 store_inventory = {}
 total_sale = 0
 
-def get_options() :
-    print("")
-    print("What would you like to do?")
-    print("S = New sale.")
-    print("I = Display sale information.")
-    print("Exit = Exit the program.")
 
 def add_item(name, price, quantity) :
-    item_details = {
-        "name": name,
-        "price": price,
-        "quantity": quantity,
-    }
-    if name in store_inventory :
-        store_inventory[name]["quantity"] += quantity
-    else :
-        store_inventory[name] = item_details
+	item_details = {
+		"name": name,
+		"price": price,
+		"quantity": quantity,
+	}
+	if name in store_inventory :
+		store_inventory[name]["quantity"] += quantity
+	else :
+		store_inventory[name] = item_details
 
-def get_sold_items_list() :
-    for name in store_inventory :
-        print(store_inventory[name]["name"])
+def get_total_sales():
+	total_sale = 0
+	for item in store_inventory :
+		total_sale += float(store_inventory[item]["price"] * store_inventory[item]["quantity"])
+	return total_sale
 
-def get_total_sale() :
-    for price in store_inventory :
-        total_sale += store_inventory[price]["price"]
-    print(total_sale)
+def get_list_items() :
+	list_items = []
+	for item in store_inventory :
+		list_items.append(store_inventory[item]["name"])
+	return list_items
+
+def get_options() :
+	print("")
+	print("What would you like to do?")
+	print("S = New sale.")
+	print("I = Display sale information.")
+	print("Exit = Exit the program.")

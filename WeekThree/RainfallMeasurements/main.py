4ed05cabbc78c98d3213ee5973ecbf4d43ef2da6
Aaron Boekhoudt <95099604+IPsychoticEnder@users.noreply.github.com>
2023-09-27 14:22:03 +0200

    RainfallMeasurements

--- WeekThree/RainfallMeasurements/main.py
+++ WeekThree/RainfallMeasurements/main.py
@@ -1,3 +1,16 @@
 from my_functions import rainfall_measurer
 
-rainfall = [0.6, 0.5, 3, 0, 0, 1.4]
+
+rainfall = rainfall_measurer.set_rainfall_measurements()
+
+print("What segment would you like? ")
+start = int(input("Start: "))
+end = int(input("End: "))
+
+weekly = rainfall_measurer.get_segment_average(rainfall, 1, 7)
+monthly = rainfall_measurer.get_segment_average(rainfall, 1, 30)
+custom = rainfall_measurer.get_segment_average(rainfall, start, end)
+
+print(f"Weekly: {weekly}")
+print(f"Monthly: {monthly}")
+print(f"Custom: {custom}")
--- WeekThree/RainfallMeasurements/my_functions/rainfall_measurer.py
+++ WeekThree/RainfallMeasurements/my_functions/rainfall_measurer.py
@@ -1,1 +1,25 @@
+rainfall = [1.2, 2.4, 1.6, 0.2, 0.6, 3.2, 1.2, 2.6, 3.0, 1.2, 1.2, 2.4, 1.6, 0.2, 0.6, 3.2, 1.2, 2.6, 3.0, 1.2, 1.2, 2.4, 1.6, 0.2, 0.6, 3.2, 1.2, 2.6, 3.0, 1.2]
 
+def set_rainfall_measurements():
+    while True:
+        print("Please enter a rainfall measurement:")
+        user_input = input("> ")
+        if user_input == "":
+            break
+        else:
+            rainfall.append(float(user_input))
+    return rainfall
+
+def get_average(numbers):
+    sum = 0
+    for number in numbers:
+        sum += number
+    return sum
+
+def get_segment_average(numbers, start, end):
+    sum = 0
+    j = 0
+    for i in range(start -1, end):
+        sum += numbers[i]
+        j +=1
+    return sum / j

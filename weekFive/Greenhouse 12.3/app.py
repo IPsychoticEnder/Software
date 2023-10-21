28ad4c0ac4a7060ed3a955f1665fc30f8ba1986b
Aaron Boekhoudt <95099604+IPsychoticEnder@users.noreply.github.com>
2023-10-21 14:22:58 +0200

    asd

--- weekFive/Greenhouse 12.3/app.py
+++ weekFive/Greenhouse 12.3/app.py
@@ -1,15 +1,32 @@
 from flask import Flask, render_template
 from functions import DateTime, Sensors, HandleLists
 
+samples = []
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    global samples
+    temperature = HandleLists.makeList("one", Sensors.getTemperature())
+    humidity = HandleLists.makeList("two", Sensors.getHumidity())
+    light_level = HandleLists.makeList("three", Sensors.getLightlevels())
+
+    samples = [temperature, humidity, light_level]
     return render_template('index.html',
-                           text = (DateTime.getTime(), DateTime.getDate()),
-                           temperature = HandleLists.makeList("one", Sensors.getTemperature()),
-                           humidity = HandleLists.makeList("two", Sensors.getHumidity()),
-                           light_level = HandleLists.makeList("three", Sensors.getLightlevels())
+                           timeDay = (DateTime.getTime(), DateTime.getDate()),
+                           temperature = temperature,
+                           humidity = humidity,
+                           light_level = light_level
                            )
+
+@app.route('/get-list', methods=['GET'])
+def getSamples():
+
+    if len(samples) == 0:
+        return {}
+    
+    send_samples = samples
+    return {'sent_samples': send_samples}
 
 app.run()

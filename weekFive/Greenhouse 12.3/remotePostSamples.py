867a279af46355d08d86d25a3e5877d30274a6ef
IpsychoticEnder <aaron.boekhoudt@gmnail.com>
2023-10-24 17:39:53 +0200

    Greenhouse arduino Done

---
+++ weekFive/Greenhouse 12.3/remotePostSamples.py
@@ -0,0 +1,15 @@
+import requests, time
+from functions import Sensors
+
+while True:
+    try:
+        temperature = Sensors.getTemperature()
+        humidity = Sensors.getHumidity()
+        light_level = Sensors.getLightLevel()
+
+        data = {'sent_temperature': temperature, 'sent_humidity': humidity, 'sent_light_level': light_level}
+        response = requests.post('http://localhost:5000/receive-sample', json=data)
+        time.sleep(5)
+    except KeyboardInterrupt:
+        print("Shutting down...")
+        break
